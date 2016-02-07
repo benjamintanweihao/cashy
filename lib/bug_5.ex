6f3d4b08bf797d16469eed48762be910e781db33
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2016-02-07 11:22:04 +0800

    Add more stuff.

--- lib/bug_5.ex
+++ lib/bug_5.ex
@@ -1,10 +1,30 @@
+# defmodule Cashy.Bug5 do
+#
+#   def convert(:sgd, :usd, amount) do
+#     amount * 0.70 
+#   end
+#
+#   # @spec amount({:value, number}) :: number
+#   def amount({:value, value}) do
+#     value
+#   end
+#
+#   def run do
+#     convert(:sgd, :usd, amount({:value, :one_million_dollars}))
+#   end
+#
+# end
+#
 defmodule Cashy.Bug5 do
 
+  @type currency() :: :sgd | :usd
+
+  @spec convert(currency, currency, number) :: float
   def convert(:sgd, :usd, amount) do
     amount * 0.70 
   end
 
-  # @spec amount({:value, number}) :: number
+  @spec amount({:value, number}) :: number
   def amount({:value, value}) do
     value
   end
@@ -14,3 +34,4 @@ defmodule Cashy.Bug5 do
   end
 
 end
+
